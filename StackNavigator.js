28abd2d148bacdbf5758c2010b5d476d906a5257
Angelo Perera <s92062815@ousl.lk>
2023-08-18 08:16:57 +0530

    updating

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./screens/Home";
 import Brands from "./screens/Brands";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
